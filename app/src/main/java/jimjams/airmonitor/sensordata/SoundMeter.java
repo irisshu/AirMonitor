ff2587c1bbd4e35496785b1364d65b30d1d5bac3
Iris Hsu <irishsuyt@gmail.com>
2015-05-04 09:21:47 -0400

    Test.

---
+++ app/src/main/java/jimjams/airmonitor/sensordata/SoundMeter.java
@@ -0,0 +1,97 @@
+
+/*
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jimjams.airmonitor.sensordata;
+
+
+import android.media.MediaRecorder;
+import android.os.Environment;
+import java.io.IOException;
+import android.util.Log;
+
+import jimjams.airmonitor.database.DBAccess;
+
+
+public class SoundMeter {
+
+    /**
+     * Database access
+     */
+    private DBAccess access = DBAccess.getDBAccess();
+    /**
+     * Reference to the SensorGenerator used to get sensor data
+     */
+    SensorDataGenerator generator = SensorDataGenerator.getInstance();
+    String data;
+
+    static final private double EMA_FILTER = 0.6;
+
+    private MediaRecorder mRecorder = null;
+    private double mEMA = 0.0;
+
+    private static String mFileName = null;
+
+    public SoundMeter() {
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
+    }
+
+    public void start() {
+        if (mRecorder == null) {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile(mFileName);
+            try {
+                mRecorder.prepare();
+                Log.d("Start Audio errror: ", "NO error");
+            }catch(IOException e){
+                //
+                Log.d("Start Audio errror: ", e.toString());
+            }
+            mRecorder.start();
+            mEMA = 0.0;
+        }
+    }
+
+    public void stop() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.reset();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public int getAmplitude() {
+        if (mRecorder != null){
+            // PREPARE sound data for database entry [DESCRIPTION:VALUE:UNITS;]
+            data  = "SoundLevel:" + Integer.toString(mRecorder.getMaxAmplitude()) + ":DB;";
+            access.updateCurrentData(generator.getData(data));
+            return  (mRecorder.getMaxAmplitude());
+        }else {
+            return 0;
+        }
+    }
+
+    public double getAmplitudeEMA() {
+        double amp = getAmplitude();
+        mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA;
+        return mEMA;
+    }
+}
